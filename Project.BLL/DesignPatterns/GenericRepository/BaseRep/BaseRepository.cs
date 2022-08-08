c4c7d6914ce5d3538e566dab6f803ac0c7b1a93b
Egehan <Egehan@DESKTOP-IPMIULM>
2022-08-08 14:47:13 +0300

    Add project files.

---
+++ Project.BLL/DesignPatterns/GenericRepository/BaseRep/BaseRepository.cs
@@ -0,0 +1,134 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.IntRep;
+using Project.BLL.DesignPatterns.SingletonPattern;
+using Project.DAL.Context;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.GenericRepository.BaseRep
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        MyContext _db;
+
+        public BaseRepository()
+        {
+            _db = DBTool.DBInstance;
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DestroyRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetLastData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetModifieds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetPassives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Select(Expression<Func<T, object>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetActive(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetActiveRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Where(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Save()
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}
